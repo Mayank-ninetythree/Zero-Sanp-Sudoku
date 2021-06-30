f746d19fd9581a748d0e83b8d762524d65a38df2
MAYANK <mayankninetythree@gmail.com>
2021-06-30 15:43:04 +0530

    Home page progress

--- app/src/main/java/com/mayank93/zerosanpsudoku/MainActivity.java
+++ app/src/main/java/com/mayank93/zerosanpsudoku/MainActivity.java
@@ -3,6 +3,7 @@ package com.mayank93.zerosanpsudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);   //hide status bar
+        getSupportActionBar().hide();       //hide action bar
     }
 }
